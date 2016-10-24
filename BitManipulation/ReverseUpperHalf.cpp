f99a2ccdba63b17f0678890f9372427ec7d35c90
Siraj Memon <siraj1@umbc.edu>
2016-10-24 00:37:37 -0400

    Updated Bit Swap Code

--- BitManipulation/ReverseUpperHalf.cpp
+++ BitManipulation/ReverseUpperHalf.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#define BITS 8
 using namespace std;
 
 /*
@@ -6,22 +7,24 @@ Reverse first four digits, keep remaining as it is!
 */
 int main() 
 {
-  char n = 166; 
-  char result = 0;
-  char orig = n;
-
-  n >>= 4;
+  unsigned short n = 15125; 
+  //0011101100010101
+  //1101110000010101
   
-  for(int i = 0; i < 4; ++i)
+  unsigned short result = 0;
+  
+  unsigned short orig = n;
+  n >>= BITS;
+  
+  for(int i = 0; i < BITS; ++i)
   {
     result = (result << 1) + ((n >> i) & 1);
   }
   
-  result <<= 4;
-  orig <<= 4;
-  orig >>= 4;
+  result <<= BITS; 
+  result |=  (orig & ((1 << BITS)- 1));
   
-  cout << (result | orig) << endl;
+  cout << result << endl;
   
   return 0;
 }
